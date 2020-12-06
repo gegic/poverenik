33a923632cb3f34ad86dee3c93f6f8f9a558728e
Haris Gegić <harry.gegic@gmail.com>
2020-12-06 23:27:46 +0100

    Init spring and create a scheme with common declarations

---
+++ poverenik/src/main/java/com/xml/team18/poverenik/PoverenikApplication.java
@@ -0,0 +1,13 @@
+package com.xml.team18.poverenik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoverenikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoverenikApplication.class, args);
+    }
+
+}
---
+++ poverenik/src/test/java/com/xml/team18/poverenik/PoverenikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xml.team18.poverenik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoverenikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
